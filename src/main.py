33d44e52186ada4a75b750441605a665264813b1
gmartins01 <10GoM@rtins10>
2023-05-07 15:59:27 +0100

    Melhor menu

--- src/main.py
+++ src/main.py
@@ -17,89 +17,92 @@ def run_simulation(desc: str, simulator: GameSimulator, iterations: int):
     simulator.print_stats()
 
 def main():
+    player1 = None
+    player2 = None
     print("ESTG IA Games Simulator\n")
     print("Day and Night Game\n")
 
-    while True:
-        print("Choose the player 1:")
-        
-        print("1 - Human")
-        print("2 - Computer (Easy)")
-        print("3 - Computer (Medium)")
-        print("4 - Computer (Hard)")
-        print("0 - Exit")
-        player1 = int(input())
-
-        if player1 == 0:
-            exit()
-        elif player1 == 1:
-            player1 = HumanDayAndNightPlayer("Human 1")
-            break
-        elif player1 == 2:
-            player1 = RandomDayAndNightPlayer("Easy 1")
-            break
-        elif player1 == 3:
-            player1 = GreedyDayAndNightPlayer("Medium 1")
-            break
-        elif player1 == 4:
-            player1 = MinimaxDayAndNightPlayer("Hard 1")
-            break
-        else:
-            print("Invalid option")
-
-    while True:    
-        print("Choose the player 2:")
-        print("1 - Human")
-        print("2 - Computer (Easy)")
-        print("3 - Computer (Medium)")
-        print("4 - Computer (Hard)")
-        print("0 - Exit")
-        player2 = int(input())
-        
-
-        if player2 == 0:
-            exit()
-        elif player2 == 1:
-            player2 = HumanDayAndNightPlayer("Human 2")
-            break
-        elif player2 == 2:
-            player2 = RandomDayAndNightPlayer("Easy 2")
-            break
-        elif player2 == 3:
-            player2 = GreedyDayAndNightPlayer("Medium 2")
-            break
-        elif player2 == 4:
-            player2 = MinimaxDayAndNightPlayer("Hard 2")
-            break
-        else:
-            print("Invalid option")
+    while player1!=0 or player2!=0:
+        while True:
+            print("Choose the player 1:")
             
-    while True:
-        print("Enter the number of games:")
-        num_iterations = int(input())
-        if num_iterations == 0:
-            exit()
-        else:
-            break
+            print("1 - Human")
+            print("2 - Computer (Easy)")
+            print("3 - Computer (Medium)")
+            print("4 - Computer (Hard)")
+            print("0 - Exit")
+            player1 = int(input())
+
+            if player1 == 0:
+                exit()
+            elif player1 == 1:
+                player1 = HumanDayAndNightPlayer("Human 1")
+                break
+            elif player1 == 2:
+                player1 = RandomDayAndNightPlayer("Easy 1")
+                break
+            elif player1 == 3:
+                player1 = GreedyDayAndNightPlayer("Medium 1")
+                break
+            elif player1 == 4:
+                player1 = MinimaxDayAndNightPlayer("Hard 1")
+                break
+            else:
+                print("Invalid option")
+
+        while True:    
+            print("Choose the player 2:")
+            print("1 - Human")
+            print("2 - Computer (Easy)")
+            print("3 - Computer (Medium)")
+            print("4 - Computer (Hard)")
+            print("0 - Exit")
+            player2 = int(input())
+            
+
+            if player2 == 0:
+                exit()
+            elif player2 == 1:
+                player2 = HumanDayAndNightPlayer("Human 2")
+                break
+            elif player2 == 2:
+                player2 = RandomDayAndNightPlayer("Easy 2")
+                break
+            elif player2 == 3:
+                player2 = GreedyDayAndNightPlayer("Medium 2")
+                break
+            elif player2 == 4:
+                player2 = MinimaxDayAndNightPlayer("Hard 2")
+                break
+            else:
+                print("Invalid option")
+                
+        while True:
+            num_iterations = int(input("Enter the number of games: "))
+            if num_iterations == 0:
+                exit()
+            else:
+                break
+        
+        while True:
+            board_size = int(input("Enter the board size: "))
+            if board_size < 11 or board_size > 19 or board_size % 2 == 0:
+                print("The size of the board must be an odd number between 11 and 19")
+            else:
+                break
+                
+        c4_simulations = [
+            {
+                "name": "Day and Night",
+                "player1": player2,
+                "player2": player1
+            }
+        ]
+
+        for sim in c4_simulations:
+            run_simulation(sim["name"], DayAndNightSimulator(sim["player1"], sim["player2"],board_size), num_iterations)
     
-    while True:
-        board_size = int(input("Enter the board size: "))
-        if board_size < 11 or board_size > 19 or board_size % 2 == 0:
-            print("The size of the board must be an odd number between 11 and 19")
-        else:
-            break
-            
-    c4_simulations = [
-        {
-            "name": "Day and Night",
-            "player1": player2,
-            "player2": player1
-        }
-    ]
 
-    for sim in c4_simulations:
-        run_simulation(sim["name"], DayAndNightSimulator(sim["player1"], sim["player2"],board_size), num_iterations)
-
-
+   
 if __name__ == "__main__":
     main()
